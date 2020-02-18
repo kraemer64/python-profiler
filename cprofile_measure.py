e816901946891ac096e13e7968632bd910d6517a
Daniel <54526557+kraemer64@users.noreply.github.com>
2020-02-18 14:05:43 +0100

    Added profileprint and profileit function

--- cprofile_measure.py
+++ cprofile_measure.py
@@ -1,1 +1,18 @@
 import cProfile
+import pstats
+
+# writing cProfile data into cmd
+def profileprint(profiler):
+    print('\n<---------CMD Function Profiling--------->\n')
+    p = pstats.Stats(profiler)
+    p.strip_dirs().sort_stats(-1).print_stats()
+
+# cProfile decorator
+def profileit(func):
+    def wrapper(*args, **kwargs):
+        profiler = cProfile.Profile()
+        retval = profiler.runcall(func, *args, **kwargs)
+        profileprint(profiler)
+        return retval
+    
+    return wrapper
